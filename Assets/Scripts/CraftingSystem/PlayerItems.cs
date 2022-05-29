a25748e796f11e937e575d0641b3261fd5d2c765
s19189 <s19189@pjwst.edu.pl>
2022-05-30 00:07:25 +0200

    finishedCraft

---
+++ Assets/Scripts/CraftingSystem/PlayerItems.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItems : MonoBehaviour
+{
+    public static int gel = 55;
+    public static int scrap = 100;
+    public static int MediGel = 1;
+}
