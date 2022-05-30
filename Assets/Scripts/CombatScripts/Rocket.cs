5d29fa7e66917b4b784cdebc289596c7c620573e
Jakub Kopczewski <s20881@pjwstk.edu.pl>
2022-05-30 14:19:35 +0200

    rocket damage

--- Assets/Scripts/CombatScripts/Rocket.cs
+++ Assets/Scripts/CombatScripts/Rocket.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     [SerializeField] GameObject explosionPrefab;
+    private float damage;
 
+    private void Start()
+    {
+        damage = GetComponent<Bullet>().damage;
+        GetComponent<Bullet>().damage = 0f;
+    }
     private void OnDestroy()
     {
         GameObject explosion = Instantiate<GameObject>(explosionPrefab, transform.position, Quaternion.identity);
-        explosion.GetComponent<Explosion>().damage = GetComponent<Bullet>().damage;
+        explosion.GetComponent<Explosion>().damage = damage;
+        explosion.GetComponent<Explosion>().hostile = GetComponent<Bullet>().hostile;
     }
 }
