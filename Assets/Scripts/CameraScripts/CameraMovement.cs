af900d8da49b63aabd6aed32c975c6069d6f8e30
MiloszMajchrowski <s20800@pjwstk.edu.pl>
2022-06-22 15:17:26 +0200

    BossRoomCamera

--- Assets/Scripts/CameraScripts/CameraMovement.cs
+++ Assets/Scripts/CameraScripts/CameraMovement.cs
@@ -8,30 +8,46 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] GameData gameData;
     private void Update()
     {
-      //  if (objectToFollow.transform.position.y- gameData.cy > 8.8) {
-       //     transform.position = new Vector3(objectToFollow.transform.position.x, objectToFollow.transform.position.y, transform.position.z);
-      //  }
-      //  else
-      //  {
-
-            if (objectToFollow.transform.position.x > gameData.cx - 1f && objectToFollow.transform.position.x < gameData.cx + 1f && objectToFollow.transform.position.y > gameData.cy - 6f && objectToFollow.transform.position.y < gameData.cy + 6f)
+        if (gameData.isbossroom)
+        {
+            if (objectToFollow.transform.position.x >  - 1.7f && objectToFollow.transform.position.x <  1.7f && objectToFollow.transform.position.y >  - 6f && objectToFollow.transform.position.y < + 6f)
             {
                 transform.position = new Vector3(objectToFollow.transform.position.x, objectToFollow.transform.position.y, transform.position.z);
             }
-            if (!(objectToFollow.transform.position.x > gameData.cx - 1f && objectToFollow.transform.position.x < gameData.cx + 1f && objectToFollow.transform.position.y > gameData.cy - 6f && objectToFollow.transform.position.y < gameData.cy + 6f))
+            if (!(objectToFollow.transform.position.x > - 1.7f && objectToFollow.transform.position.x <  1.7f && objectToFollow.transform.position.y >  - 6f && objectToFollow.transform.position.y <   6f))
             {
                 transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             }
-            if (!(objectToFollow.transform.position.x > gameData.cx - 1f && objectToFollow.transform.position.x < gameData.cx + 1f) && objectToFollow.transform.position.y > gameData.cy - 6f && objectToFollow.transform.position.y < gameData.cy + 6f)
+            if (!(objectToFollow.transform.position.x > - 1.7f && objectToFollow.transform.position.x <  1.7f) && objectToFollow.transform.position.y >  - 6f && objectToFollow.transform.position.y <   6f)
             {
                 transform.position = new Vector3(transform.position.x, objectToFollow.transform.position.y, transform.position.z);
             }
-            if (objectToFollow.transform.position.x > gameData.cx - 1f && objectToFollow.transform.position.x < gameData.cx + 1f && !(objectToFollow.transform.position.y > gameData.cy - 6f && objectToFollow.transform.position.y < gameData.cy + 6f))
+            if (objectToFollow.transform.position.x >  - 1.7f && objectToFollow.transform.position.x <  1.7f && !(objectToFollow.transform.position.y >  - 6f && objectToFollow.transform.position.y <   6f))
             {
                 transform.position = new Vector3(objectToFollow.transform.position.x, transform.position.y, transform.position.z);
             }
+        }
+        else
+        {
+            if (objectToFollow.transform.position.x > gameData.cx - 1.7f && objectToFollow.transform.position.x < gameData.cx + 1.7f && objectToFollow.transform.position.y > gameData.cy - 6f && objectToFollow.transform.position.y < gameData.cy + 6f)
+            {
+                transform.position = new Vector3(objectToFollow.transform.position.x, objectToFollow.transform.position.y, transform.position.z);
+            }
+            if (!(objectToFollow.transform.position.x > gameData.cx - 1.7f && objectToFollow.transform.position.x < gameData.cx + 1.7f && objectToFollow.transform.position.y > gameData.cy - 6f && objectToFollow.transform.position.y < gameData.cy + 6f))
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            }
+            if (!(objectToFollow.transform.position.x > gameData.cx - 1.7f && objectToFollow.transform.position.x < gameData.cx + 1.7f) && objectToFollow.transform.position.y > gameData.cy - 6f && objectToFollow.transform.position.y < gameData.cy + 6f)
+            {
+                transform.position = new Vector3(transform.position.x, objectToFollow.transform.position.y, transform.position.z);
+            }
+            if (objectToFollow.transform.position.x > gameData.cx - 1.7f && objectToFollow.transform.position.x < gameData.cx + 1.7f && !(objectToFollow.transform.position.y > gameData.cy - 6f && objectToFollow.transform.position.y < gameData.cy + 6f))
+            {
+                transform.position = new Vector3(objectToFollow.transform.position.x, transform.position.y, transform.position.z);
+            }
+        }
+            
 
-       // }
 
 
     }
